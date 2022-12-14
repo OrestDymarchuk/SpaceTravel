153eb9aca171d37fb901159b05627cce4d25c84a
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-12-15 03:23:12 +0400

    Changed from application.properties to hibernate.properties

--- app/src/main/java/space_travel/preferences/PropertiesFileReader.java
+++ app/src/main/java/space_travel/preferences/PropertiesFileReader.java
@@ -1,14 +1,17 @@
 package space_travel.preferences;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFileReader {
     private static final Properties PROPERTIES = new Properties();
     public String getPath() throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("application.properties");
-        PROPERTIES.load(fileInputStream);
-        return PROPERTIES.getProperty("path");
+        InputStream inputStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream("hibernate.properties");
+
+        PROPERTIES.load(inputStream);
+        return PROPERTIES.getProperty("hibernate.connection.url");
     }
 }
