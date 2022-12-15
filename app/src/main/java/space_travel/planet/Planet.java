7d78e58f744d4c3683c331cfaad608e4df598bd0
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-12-15 05:48:44 +0400

    Removed unused import

--- app/src/main/java/space_travel/planet/Planet.java
+++ app/src/main/java/space_travel/planet/Planet.java
@@ -2,12 +2,10 @@ package space_travel.planet;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
 public class Planet {
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private String id;
     @Column()
