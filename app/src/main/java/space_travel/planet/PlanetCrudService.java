168c1bfe959da04544b6ea68f6013e08760aa61c
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-12-15 05:08:02 +0400

    Removed selectPlanetById() method. Added getPlanetById(), updatePlanetById(), getAllPlanets() methods

--- app/src/main/java/space_travel/planet/PlanetCrudService.java
+++ app/src/main/java/space_travel/planet/PlanetCrudService.java
@@ -5,46 +5,70 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import space_travel.hibernate_util.HibernateUtil;
 
+import java.util.List;
+
 public class PlanetCrudService {
     HibernateUtil util = HibernateUtil.getInstance();
 
-    private String selectPlanetById(String id) {
-        try (Session session = util.getSessionFactory().openSession();) {
-            Planet planet;
-            Query<Planet> planetQuery = session.createQuery(
-                    "from Planet where id = :id",
-                    Planet.class
-            );
-
-            planetQuery.setParameter("id", id);
-            if (planetQuery.list().isEmpty()) {
-                return null;
+    public void getPlanetById(String id) {
+        try (Session session = util.getSessionFactory().openSession()) {
+            Planet existingPlanet = session.get(Planet.class, id);
+            if (existingPlanet == null) {
+                System.out.println("The planet " + id + " does not exists.");
             } else {
-                planet = planetQuery.getSingleResult();
-                return planet.getId();
+                System.out.println(existingPlanet);
             }
         }
     }
 
+    public void updatePlanetById(String id, String name) {
+        try (Session session = util.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Planet existingPlanet = session.get(Planet.class, id);
+            if (existingPlanet == null) {
+                System.out.println("The planet " + id + " does not exists.");
+            } else {
+                existingPlanet.setName(name);
+                session.persist(existingPlanet);
+                transaction.commit();
+                System.out.println(existingPlanet);
+            }
+        }
+    }
+
+    public List<Planet> getAllPlanets() {
+        List<Planet> planets;
+        try (Session session = util.getSessionFactory().openSession()) {
+            Query<Planet> planetQuery = session.createQuery(
+                    "from Planet",
+                    Planet.class
+            );
+
+            planets = planetQuery.list();
+            return planets;
+        }
+    }
+
     public void createNewPlanet(String id, String name) {
-        if(id.equals(new PlanetCrudService().selectPlanetById(id))){
-            System.out.println("The planet already exists.");
-        }
-        else if(!id.matches("[A-Z0-9]*$")){
+        if (!id.matches("[A-Z0-9]*$")) {
             System.out.println("The planet ID must contain capital letters without special characters.");
-        }
-        else if (name.length() < 1) {
+        } else if (name.length() < 1) {
             System.out.println("The name of the planet must contain at least one character.");
 
         } else {
             try (Session session = util.getSessionFactory().openSession()) {
                 Transaction transaction = session.beginTransaction();
-                Planet newPlanet = new Planet();
-                newPlanet.setId(id);
-                newPlanet.setName(name);
-                session.persist(newPlanet);
-                System.out.println(newPlanet);
-                transaction.commit();
+                Planet planet = session.get(Planet.class, id);
+                if (planet != null) {
+                    System.out.println("The planet already exists.");
+                } else {
+                    Planet newPlanet = new Planet();
+                    newPlanet.setId(id);
+                    newPlanet.setName(name);
+                    session.persist(newPlanet);
+                    System.out.println(newPlanet);
+                    transaction.commit();
+                }
             }
         }
     }
