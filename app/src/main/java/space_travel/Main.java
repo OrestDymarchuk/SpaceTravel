ad05d8cdc3e70460722fd4611ff6a59be71a39b7
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-12-15 09:15:00 +0400

    Cleaned the main class

--- app/src/main/java/space_travel/Main.java
+++ app/src/main/java/space_travel/Main.java
@@ -1,46 +1,9 @@
 package space_travel;
 
-import space_travel.client.ClientCrudService;
-import space_travel.planet.PlanetCrudService;
+import space_travel.information_printer.InformationCenter;
 
 public class Main {
     public static void main(String[] args) {
-        ClientCrudService clientCrudService = new ClientCrudService();
-        PlanetCrudService planetCrudService = new PlanetCrudService();
-
-        // Create a client with restrictions
-        clientCrudService.createNewClient("");
-
-        // Create a client without restrictions
-        clientCrudService.createNewClient("Cat");
-
-        // Delete a non-existent client
-        clientCrudService.deleteClient(99);
-
-        // Delete a existent client
-        clientCrudService.deleteClient(11);
-
-
-        // Create a existent ID planet
-        planetCrudService.createNewPlanet("MARS", "Mars");
-
-        // Create a planet with ID restrictions
-        planetCrudService.createNewPlanet("Mars", "Mars");
-
-        // Create a planet with ID restrictions
-        planetCrudService.createNewPlanet("#$%^&", "Mars");
-
-        // Create a planet with Name restrictions
-        planetCrudService.createNewPlanet("TEST", "");
-
-        // Create a planet without restrictions
-        planetCrudService.createNewPlanet("TEST", "Test");
-
-        // Delete a existent planet
-        planetCrudService.deletePlanet("TEST");
-
-        // Delete a non-existent planet
-        planetCrudService.deletePlanet("TEST");
-
+        new InformationCenter().startApp();
     }
 }
