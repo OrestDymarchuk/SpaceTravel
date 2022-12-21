02ab907bc82fde7bcec587869f208675a0e6c436
Orest Dymarchuk <oliverkromvel2016@gmail.com>
2022-12-21 04:13:51 +0400

    Removed trash from main

--- app/src/main/java/space_travel/Main.java
+++ app/src/main/java/space_travel/Main.java
@@ -3,18 +3,5 @@ package space_travel;
 import space_travel.bot.InformationCenterBot;
 
 public class Main {
-    public static void main(String[] args) {
-
-        new InformationCenterBot().startApp();
-
-
-//        new TicketCrudService().getTicketById(12);
-//        new TicketCrudService().getAllTickets().forEach(System.out::println);
-//        new ClientCrudService().getAllClients().forEach(System.out::println);
-//        new TicketCrudService().updateTicket(12, "VEN", "JUP");
-//        new TicketCrudService().deleteTicketById(11);
-//        String currentDateAndTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-//        new TicketCrudService().createTicket(currentDateAndTime, 10, "MARS", "EARTH");
-
-    }
+    public static void main(String[] args) {new InformationCenterBot().startApp();}
 }
